dd675796b987a3c9bc48770cf420572b2f77dd97
ahmed salah <ahmed.salah200555@gmail.com>
2025-09-26 09:50:30 +0300

    Change Email and Password Dtos

---
+++ FlowForge.Core/DTO/ChangeEmailDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowForge.Core.DTO
+{
+    public class ChangeEmailDTO
+    {
+        public string CurrentEmail { get; set; }
+        public string NewEmail { get; set; }
+    }
+}
